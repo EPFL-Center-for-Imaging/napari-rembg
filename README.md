8ebc69b2b3e3dddce4af1106cd0572609f2f0712
Mallory Wittwer <mallory.wittwer@epfl.ch>
2023-09-21 21:56:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# napari-rembg
+
+Remove the background of images using AI in Napari. This plugin is based on the [rembg](https://github.com/danielgatis/rembg) project.
+
+<!-- <p align="center">
+    <img src="assets/gif01.gif" height="400">
+</p> -->
+
+----------------------------------
+
+This [napari] plugin was generated with [Cookiecutter] using [@napari]'s [cookiecutter-napari-plugin] template.
+
+## Installation
+
+You can install `napari-rembg` via [pip]:
+
+    pip install napari-rembg
+
+## Contributing
+
+Contributions are very welcome. Tests can be run with [tox], please ensure
+the coverage at least stays the same before you submit a pull request.
+
+## License
+
+Distributed under the terms of the [BSD-3] license,
+"napari-label-focus" is free and open source software
+
+## Issues
+
+If you encounter any problems, please file an issue along with a detailed description.
+
+[napari]: https://github.com/napari/napari
+[Cookiecutter]: https://github.com/audreyr/cookiecutter
+[@napari]: https://github.com/napari
+[MIT]: http://opensource.org/licenses/MIT
+[BSD-3]: http://opensource.org/licenses/BSD-3-Clause
+[GNU GPL v3.0]: http://www.gnu.org/licenses/gpl-3.0.txt
+[GNU LGPL v3.0]: http://www.gnu.org/licenses/lgpl-3.0.txt
+[Apache Software License 2.0]: http://www.apache.org/licenses/LICENSE-2.0
+[Mozilla Public License 2.0]: https://www.mozilla.org/media/MPL/2.0/index.txt
+[cookiecutter-napari-plugin]: https://github.com/napari/cookiecutter-napari-plugin
+
+[napari]: https://github.com/napari/napari
+[tox]: https://tox.readthedocs.io/en/latest/
+[pip]: https://pypi.org/project/pip/
+[PyPI]: https://pypi.org/
---
+++ src/napari_rembg/_widget.py
@@ -0,0 +1,78 @@
+from typing import TYPE_CHECKING
+import numpy as np
+import PIL
+import rembg
+from napari.qt.threading import thread_worker
+import napari.layers
+from napari_tools_menu import register_dock_widget
+from qtpy.QtWidgets import QComboBox, QGridLayout, QWidget, QSizePolicy, QLabel, QPushButton, QProgressBar
+from qtpy.QtCore import Qt
+
+if TYPE_CHECKING:
+    import napari
+
+def rembg_predict(image: np.ndarray) -> np.ndarray:
+    """Binary segmentation using rembg."""
+    seg = np.array(rembg.remove(PIL.Image.fromarray(image)))
+    seg = np.mean(seg, axis=2)
+    seg[seg != 0] = 1
+    seg = seg.astype(np.uint8)
+    return seg
+
+@register_dock_widget(menu="RemBG > Remove background")
+class RemBGWidget(QWidget):
+    def __init__(self, napari_viewer):
+        super().__init__()
+        self.viewer = napari_viewer
+
+        # Layout
+        grid_layout = QGridLayout()
+        grid_layout.setAlignment(Qt.AlignTop)
+        self.setLayout(grid_layout)
+
+        # Image
+        self.cb_image = QComboBox()
+        self.cb_image.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        grid_layout.addWidget(QLabel("Image (2D RGB or gray)", self), 0, 0)
+        grid_layout.addWidget(self.cb_image, 0, 1)
+
+        # Compute button
+        self.remove_background_btn = QPushButton("Remove background", self)
+        self.remove_background_btn.clicked.connect(self._trigger_remove_background)
+        grid_layout.addWidget(self.remove_background_btn, 1, 0, 1, 2)
+
+        # Progress bar
+        self.pbar = QProgressBar(self, minimum=0, maximum=1)
+        self.pbar.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        grid_layout.addWidget(self.pbar, 2, 0, 1, 2)
+
+        # Setup layer callbacks
+        self.viewer.layers.events.inserted.connect(
+            lambda e: e.value.events.name.connect(self._on_layer_change)
+        )
+        self.viewer.layers.events.inserted.connect(self._on_layer_change)
+        self.viewer.layers.events.removed.connect(self._on_layer_change)
+        self._on_layer_change(None)
+
+    def _on_layer_change(self, e):
+        self.cb_image.clear()
+        for x in self.viewer.layers:
+            if isinstance(x, napari.layers.Image):
+                if (len(x.data.shape) == 2) | x.rgb:
+                    self.cb_image.addItem(x.name, x.data)
+
+    @thread_worker
+    def _remove_background(self) -> np.ndarray:
+        self.image = self.cb_image.currentData()
+        segmentation = rembg_predict(self.image)
+        return segmentation
+    
+    def _trigger_remove_background(self):
+        self.pbar.setMaximum(0)
+        worker = self._remove_background()
+        worker.returned.connect(self._thread_returned)
+        worker.start()
+
+    def _thread_returned(self, segmentation):
+        self.viewer.add_labels(segmentation, name='mask')
+        self.pbar.setMaximum(1)
